75224431d5c222ae8517195be5bc508aff2e6f94
Mazurel <mateusz.mazur@yahoo.com>
2021-06-27 22:23:04 +0200

    General improvements and some work on soundboard

---
+++ rpi/server/programs/soundboard/soundboard.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import os
+from os import path
+
+import toster
+
+# TODO: Change it to /var/db/toster-soundboard on rpi
+TRACKS_PATH = "/tmp/db/toster-soundboard"
+
+try:
+    os.makedirs(TRACKS_PATH)
+except FileExistsError:
+    # Create folder only if it does not exist
+    pass
+
+
+def onRequest(req):
+    msg = req["msg"]
+    #print(msg, file=sys.stderr)
+    
+    if "play" in msg:
+        soundTrack = msg["play"]
+        soundTrackPath = path.join(TRACKS_PATH, soundTrack)
+
+        # Play sound via omxplayer
+        os.system(f"omxplayer --no-keys {soundTrackPath}")
+    elif "list" in msg and msg["list"]:
+        tracks = os.listdir(TRACKS_PATH)
+
+        # Filter invalid filetypes
+        toster.sendResponse(req, { "tracks": tracks })
+    pass
+
+toster.registerRequestCallback(onRequest)
+toster.start(block=True)
